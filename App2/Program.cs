0fcf79852fb023ac81749cd24d2c489d11c0c482
Hazel Granados <hazelbgranados@gmail.com>
2022-07-30 09:32:25 -0900

    Actualizacion

---
+++ App2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 27;
+            var suma = num1 + num2;
+            int edad = 17;
+            string name = "Brendaly";
+            //Impresion de datos
+            Console.WriteLine(suma);
+            Console.WriteLine("Su nombre es: {0} y su edad es: {1}", name, edad);
+        }
+    }
+}
