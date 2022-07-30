21b26a1143d795aa0d7a65fbe0a89e4e471a5611
Hazel Granados <hazelbgranados@gmail.com>
2022-07-30 12:20:26 -0900

    Programa cinco

---
+++ App5/ProgramBase.cs
@@ -0,0 +1,18 @@
+﻿namespace App5
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            int num2 = int.Parse(Console.ReadLine());
+            int v = num1 + num2;
+            var suma = v;
+            int x = num1 - num2;
+            var resta = x;
+            Console.WriteLine("La suma de los numeros es: {0} la resta de los numeros es: {1} ", suma, resta);
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Ingrese un numero: ");
+            int num1 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Ingrese otro numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+            var suma = num1 + num2;
+            double resta = num1 - num2;
+            double multiplicacion = num1 * num2;
+            double decimal division = num1 / num2;
+            System.Console.WriteLine("El resultado de las operaciones son  suma: {0}, resta: {1}, multiplicacion: {2}, division: {3} , ", suma, resta , multiplicacion , division);
+           
+        }
+    }
+}
