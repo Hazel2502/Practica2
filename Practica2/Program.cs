ea7ec6497769d987691b2aa99b6ce9b3a89b8cf1
Hazel Granados <hazelbgranados@gmail.com>
2022-07-30 09:16:19 -0900

    Agregar archivos de proyecto.

---
+++ Practica2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int edad = 17;
+            String name = "Brendaly";
+            Console.WriteLine("Su nombre es: " + name + " y su edad es: " + edad);
+        }
+    }
+}
